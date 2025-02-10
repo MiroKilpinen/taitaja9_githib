a64e2dbbd64943fb0465dbdad76c8e48763f7383
olvifarrand <olivia.farrand@edu.salpaus.fi>
2025-02-10 13:31:07 +0200

    startinggggg theeeeeeee login

---
+++ client/taitaja9/src/components/login.js
@@ -0,0 +1,17 @@
+import "../styles/login.css";
+
+<div class="CONTAINER">
+    <h2>Kirjaudu sisään</h2>
+    <div class="flex-div">  
+        <p>Nimi: </p> 
+        <input type="text"></input>
+    </div>
+    <div class="flex-div">
+        <p>Salasana: </p> 
+        <input type="text"></input>
+    </div>
+    <div class="flex-div">
+        <input type="submit">Jatka kirjautumatta</input>
+        <input type="submit"> kirjaudu sisään</input>
+    </div>
+</div>

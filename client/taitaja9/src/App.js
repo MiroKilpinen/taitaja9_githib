fd54ef232bd5013dd84df9e10ab82eae5552b381
Aada-koodari <aada.rauhala@edu.salpaus.fi>
2025-02-10 13:17:48 +0200

    did

--- client/taitaja9/src/App.js
+++ client/taitaja9/src/App.js
@@ -13,10 +13,27 @@ function App() {
       <Navbar />
       <div className="App-body">
         <div className="App-info">
-          <span className="App-info-1">Kilpailun nimi</span>
-          <span className="App-info-1">rasti #</span>
-          <span className="App-info-1">kilpailun aika</span>
+          <span className="App-info-1"><span className="#">Kilpailun nimi</span></span>
+          <span className="App-info-1">rasti <span className="#">#</span></span>
+          <span className="App-info-1"><span className="#">rastin aika</span></span>
           <button className="App-info-2">Filter</button>
+        </div>
+        <div className="team-flex">
+          <div className="team-container">
+              <div className="team-num">
+                <p>num #</p>
+              </div>
+              <div className="team-nimet">
+                <p>Tiimi nimi</p>
+                <p>Koulun nimi</p>
+              </div>
+              <div className="team-rasti-aika">
+                <p>rasti aika</p>
+              </div>
+              <div className="team-koko-aika">
+                <p>kokonaisaika</p>
+              </div>
+          </div>
         </div>
       </div>
     </div>

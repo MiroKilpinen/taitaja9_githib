c17e8724e76da62f827e6638ed9b879157718de9
MiroKilpinen <miro.kilpinen@edu.salpaus.fi>
2025-02-10 10:44:04 +0200

    mergessä vahingossa tuli vahingossa extra line niin poistin sen

--- client/taitaja9/src/pages/users.js
+++ client/taitaja9/src/pages/users.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../styles/users.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/footr";
-import Footer from "../components/footr";
 
 function Users() {
   return (

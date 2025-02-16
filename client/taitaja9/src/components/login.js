b0a6c8b162d3376d082f0ee991ed72a3ddd1c530
Olivia Farrand <Olivia.Farrand@edu.salpaus.fi>
2025-02-16 16:19:02 +0200

    argh

--- client/taitaja9/src/components/login.js
+++ client/taitaja9/src/components/login.js
@@ -1,17 +1,39 @@
 import "../styles/login.css";
+import React, { useState } from 'react';
 
-<div class="CONTAINER">
-    <h2>Kirjaudu sisään</h2>
-    <div class="flex-div">  
-        <p>Nimi: </p> 
-        <input type="text"></input>
-    </div>
-    <div class="flex-div">
-        <p>Salasana: </p> 
-        <input type="text"></input>
-    </div>
-    <div class="flex-div">
-        <input type="submit">Jatka kirjautumatta</input>
-        <input type="submit"> kirjaudu sisään</input>
-    </div>
-</div>
+const loginPopup = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openLogin = () => setIsOpen(true);
+    const closeLogin = () => setIsOpen(false);
+
+
+
+    return (
+        <div>
+        <button onClick={openLogin}>Login</button>
+        
+        {isOpen && (
+            <div class="CONTAINER">
+                <button onClick={closeLogin}><i class="fi fi-br-cross"></i></button>
+                <h2>Kirjaudu sisään</h2>
+                <div class="flex-div">  
+                    <p>Nimi: </p> 
+                    <input type="text"></input>
+                </div>
+                <div class="flex-div">
+                    <p>Salasana: </p> 
+                    <input type="text"></input>
+                </div>
+                <div class="flex-div">
+                    <input type="submit">Jatka kirjautumatta</input>
+                    <input type="submit"> kirjaudu sisään</input>
+                </div>
+            </div>
+        )}
+        </div>
+    );
+};
+
+
+export default loginPopup;

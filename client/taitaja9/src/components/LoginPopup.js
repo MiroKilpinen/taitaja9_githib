1b3cdc66740d83b309526586027fba5893bc9d73
olvifarrand <olivia.farrand@edu.salpaus.fi>
2025-02-19 14:31:52 +0200

    uhhhhhh joo tota.

--- client/taitaja9/src/components/LoginPopup.js
+++ client/taitaja9/src/components/LoginPopup.js
@@ -5,42 +5,59 @@ import React, { useState } from 'react';
 const LoginPopup = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const openLogin = () => setIsOpen(true);
+    //napit
+    const openLogin = () => setIsOpen(true);    
     const closeLogin = () => setIsOpen(false);
     const continueWOLogin = () => setIsOpen(false);
     
+    const [inputName, setInputName] = useState("");
+    const [inputPassword, setInputPassword] = useState("");
+
+    //tää nyt pistää Login function triggeraa kokoajan
+    function handleNameChange(e) {
+        setInputName(e.target.value);
+      }
+      function handlePasswordChange(e) {
+        setInputPassword(e.target.value);
+    }
+
+    //see if it works
     function Login(inputName, inputPassword) {
         console.log(inputName);
-        if (inputName.lenght < 0 && inputPassword.lenght < 0) {
-            console.log("ei voi olla tyhjä dumbass");
-        } else if (inputName === undefined && inputPassword === undefined) {
-            console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaargh");
-        } else {
-            console.log("kirjauduttu or smth");
-        }
+        console.log(inputPassword);
+        //setIsOpen(false); //jos tää on nyt päällä nykysessä koodissa, login ei avaudu ollenkaa, (tarkotus ois et sulkeutuu ku on kirjauduttu successfully)
     }
 
     return (
         <div>
+        {/*button navbarissa*/}
         <button onClick={openLogin} class="openLogin">Kirjaudu sisään</button>
         
         {isOpen && (
             <div class="CONTAINER">
                 <div class="flex-div">
-                    <h2>Kirjaudu sisään</h2>
+                    <h2 class="kirjauduSisText">Kirjaudu sisään</h2>
                     <button onClick={closeLogin} class="closeLogin">&#10006;</button>
                 </div>
                 <div class="flex-div">  
                     <p class="contents">Nimi: </p> 
-                    <input id="inputName" name="inputName"/>
+                    <input 
+                    onChange={handleNameChange} 
+                    id="inputName"
+                    value={inputName}
+                    />
                 </div>
                  <div class="flex-div">
                      <p class="contents">Salasana: </p> 
-                    <input id="inputPassword" name="inputPassword"/>
+                    <input 
+                    onChange={handlePasswordChange} 
+                    id="inputPassword"
+                    value={inputPassword}
+                    />
                 </div>
                 <div class="flex-div">
-                    <button class="loginBtns" onClick={continueWOLogin}>Jatka kirjautumatta</button>
-                    <button class="loginBtns" onClick={Login}> kirjaudu sisään</button>
+                    <button class="loginBtns" onClick={continueWOLogin}>Jatka kirjautumatta</button>{/*basically sama ku close login, mut hes here for astethic purposes :3*/}
+                    <button class="loginBtns" onClick={Login(inputName, inputPassword)}> kirjaudu sisään</button>
                 </div>
             </div>
         )}
